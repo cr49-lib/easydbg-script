7694d7fdfce05a8eb34957176d1961bd3213d3a5
381848900@qq.com <asahichyan33@gmail.com>
2024-11-19 09:56:00 +0800

    first

---
+++ easydbg_script/src/easydbg_script.cpp
@@ -0,0 +1,17 @@
+﻿// easydbg_script.cpp: 定义应用程序的入口点。
+//
+
+#include "easydbg_script.h"
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+using namespace std;
+
+int add(int i, int j) {
+    return i + j;
+}
+
+PYBIND11_MODULE(example, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring123
+
+    m.def("add", &add, "A function that adds two numbers");
+}
